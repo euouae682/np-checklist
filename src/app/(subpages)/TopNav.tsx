2983ef3a610d93c80f41a5fc1ca355eb48f456ff
euouae682 <jasonuzhao682@gmail.com>
2023-10-23 20:37:35 -0700

    Created layout for TopNav element

--- src/app/(subpages)/TopNav.tsx
+++ src/app/(subpages)/TopNav.tsx
@@ -1,7 +1,13 @@
 export default function TopNav() {
     return (
-        <nav>
-            <h1>This is a top nav</h1>
+        <nav className="w-full h-24">
+            <ul className="h-full flex flex-row">
+                <li className="flex items-center justify-center w-1/6">Main Header</li>
+                <li className="flex items-center justify-center w-1/12">Explore</li>
+                <li className="flex items-center justify-center w-1/12">Track</li>
+                <li className="flex items-center justify-center w-1/12">Plan</li>
+                <li className="flex items-center justify-center w-1/12 ml-auto">Profile</li>
+            </ul>
         </nav>
     )
 }
