165184296b66237b4de32ff55d300ae81a6516f0
euouae682 <jasonuzhao682@gmail.com>
2023-11-16 20:56:56 -0800

    Styled ListHeader component

--- src/app/(subpages)/track/ListHeader.tsx
+++ src/app/(subpages)/track/ListHeader.tsx
@@ -1,12 +1,12 @@
 export default function ListHeader() {
     return (
-        <div>
-            <h2>U.S. National Park Checklist</h2>
-            <h4>List Description Here</h4>
-            <h2>Completion % (XX/YY)</h2>
-            <div>
-                <button>Show Stats</button>
-                <button>Add to List</button>
+        <div className="bg-white shadow-lg rounded-3xl grid grid-cols-5 grid-rows-2 py-4">
+            <h2 className="col-span-3 p-4 pl-10">U.S. National Park Checklist</h2>
+            <h2 className="col-span-2 p-4">Completion % (XX/YY)</h2>
+            <h5 className="col-span-3 p-4 pl-10">List Description Here</h5>
+            <div className="col-span-2 flex p-4 gap-16">
+                <button className="bg-button-blue rounded-full text-white py-2 px-8">Show Stats</button>
+                <button className="bg-button-blue rounded-full text-white py-2 px-8">Add to List</button>
             </div>
         </div>
     )
