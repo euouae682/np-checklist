b6b17844c38532481f7fa0210393af9e0aa82880
euouae682 <jasonuzhao682@gmail.com>
2023-11-19 15:34:37 -0800

    added basic interactivity to showStats

--- src/app/(subpages)/track/ListHeader.tsx
+++ src/app/(subpages)/track/ListHeader.tsx
@@ -1,11 +1,11 @@
-const ListHeader = () => {
+const ListHeader = (props: any) => {
     return (
         <div className="bg-white shadow-lg rounded-3xl grid grid-cols-5 grid-rows-2 py-4">
             <h2 className="col-span-3 p-4 pl-10">U.S. National Park Checklist</h2>
             <h2 className="col-span-2 p-4">Completion % (XX/YY)</h2>
             <h5 className="col-span-3 p-4 pl-10">List Description Here</h5>
             <div className="col-span-2 flex p-4 gap-16">
-                <button className="bg-button-blue hover:bg-button-blue-hover rounded-full text-white py-2 px-8 transition">Show Stats</button>
+                <button onClick={ props.handleStatsClick } className="bg-button-blue hover:bg-button-blue-hover rounded-full text-white py-2 px-8 transition">Show Stats</button>
                 <button className="bg-button-blue hover:bg-button-blue-hover rounded-full text-white py-2 px-8 transition">Add to List</button>
             </div>
         </div>
--- src/app/(subpages)/track/page.tsx
+++ src/app/(subpages)/track/page.tsx
@@ -1,15 +1,24 @@
+'use client';
+
+import { useState } from "react";
 import ListHeader from "./ListHeader";
 import ParkList from "./ParkList";
 import ListStats from "./ListStats";
 
 const Track = () => {
+    const [showStats, setShowStats] = useState(false);
+
+    const toggleShowStats = () => {
+        setShowStats(!showStats);
+    }
+
     return (
-        <main className="p-10 grid grid-rows-2 grid-cols-4 gap-10 bg-pastel-green">
-            <div className="col-span-3 flex flex-col gap-10">
-                <ListHeader />
+        <main className="p-10 grid grid-rows-2 grid-cols-4 gap-10 bg-pastel-green relative">
+            <div className={`${showStats ? 'col-span-3' : 'col-span-4'} flex flex-col gap-10`}>
+                <ListHeader handleStatsClick={ toggleShowStats } />
                 <ParkList />
             </div>
-            <ListStats />
+            { showStats && <ListStats /> }
         </main>
     );
 }
