346591a5c10323a51a604023677a1042cd930a47
euouae682 <jasonuzhao682@gmail.com>
2023-12-07 15:40:19 -0800

    Added fade transition for AddPark component

--- src/app/(subpages)/track/AddPark.tsx
+++ src/app/(subpages)/track/AddPark.tsx
@@ -4,7 +4,7 @@ interface AddParkProps {
 
 const AddPark = ({showAdd}: AddParkProps) => {
     return (
-        <div className={`${showAdd ? 'opacity-100 visible' : 'opacity-0 invisible'}  bg-white shadow-lg rounded-3xl fixed w-1/4 left-1/2 top-1/2 translate-x-[-50%] translate-y-[-50%] p-8 transition-opacity`}>
+        <div className={`${showAdd ? 'opacity-100 visible fadeOut' : 'opacity-0 invisible fadeIn'}  bg-white shadow-lg rounded-3xl fixed w-1/4 left-1/2 top-1/2 translate-x-[-50%] translate-y-[-50%] p-8`}>
             <h2 className="pb-4">Add to List</h2>
             <hr className="border-slate-300 border" />
             <form>
@@ -15,7 +15,7 @@ const AddPark = ({showAdd}: AddParkProps) => {
                 <label htmlFor="loc" className="pt-2" >Location</label><br />
                 <input type="text" id="loc" className="shadow-inner rounded-md h-8 w-full py-4" /><br />
                 <div className="text-center py-6">
-                <input type="submit" className="bg-button-blue hover:bg-button-blue-hover rounded-full text-white py-2 px-8 transition" />
+                    <input type="submit" className="bg-button-blue hover:bg-button-blue-hover rounded-full text-white py-2 px-8 transition" />
                 </div>
             </form>
         </div>
