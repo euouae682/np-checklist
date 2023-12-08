a3a8d1bedb7193b3e4cb280b78ad07bc2f92b2f8
euouae682 <jasonuzhao682@gmail.com>
2023-12-07 17:10:01 -0800

    Edited ParkItem to give space for MoreInfo

--- src/app/(subpages)/track/ParkItem.tsx
+++ src/app/(subpages)/track/ParkItem.tsx
@@ -6,12 +6,14 @@ interface ParkProps {
 }
 
 const ParkItem = ({data, color}: ParkProps) => {
-    return <div className="border-slate-400 border-2 rounded-xl my-4 p-4 relative bg-no-repeat bg-right bg-[length:30em]" 
+    return <div className="border-slate-400 rounded-xl border-2 my-4 relative">
+        <div className="p-4 rounded-xl bg-no-repeat bg-right bg-[length:30em]" 
     style={{backgroundImage: `linear-gradient(to right, rgba(255, 255, 255, 1), 25%, rgba(0, 0, 0, 0)), url(/parks/${ data.image })`}}>
-        <h4 className="leading-8">{ data.name }</h4>
-        <h5 className="leading-8">{ data.state }</h5>
-        <a>Show more</a>
-        <div className={`${color} h-full w-10 absolute right-0 top-0 rounded-r-lg`}></div>
+            <h4 className="leading-8">{ data.name }</h4>
+            <h5 className="leading-8">{ data.state }</h5>
+            <a>Show more</a>
+            <div className={`${color} h-full w-10 absolute right-0 top-0 rounded-r-lg`}></div>
+        </div>
     </div>;
 }
 
