1a1fec4c7b52471ebb61f75f2953037201782f73
euouae682 <jasonuzhao682@gmail.com>
2024-01-02 10:51:18 -0800

    lessened the shadow on TopNav

--- src/app/(subpages)/TopNav.tsx
+++ src/app/(subpages)/TopNav.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const TopNav = () => {
     return (
-        <nav className="w-full h-16 fixed shadow-dark z-10 bg-white top-0">
+        <nav className="w-full h-16 fixed shadow-md z-10 bg-white top-0">
             <ul className="h-full flex flex-row">
                 <li className="flex items-center justify-center w-1/6 cursor-pointer"><h1><Link className="text-gray-900 text-[28px] font-normal" href="/">NP Checklist</Link></h1></li>
                 <li className="flex items-center justify-center w-1/12 font-light cursor-pointer hover:bg-slight-gray transition"><h3><Link className="text-gray-900 text-[28px] font-light" href="/explore">Explore</Link></h3></li>
